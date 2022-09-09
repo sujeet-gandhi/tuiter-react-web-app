5ec2e7c7e20f71db6c0b58ac7ddbe37daa03ef68
sujeet-gandhi <sujeetgandhi014@gmail.com>
2022-09-09 01:12:28 -0700

    add email

--- src/App.js
+++ src/App.js
@@ -16,7 +16,8 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a><br/>
+        gandhi.su@northeastern.edu
       </header>
     </div>
   );
